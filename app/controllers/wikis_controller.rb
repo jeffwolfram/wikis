6f857ce910a59c4fd79c4a4393f3a7f709f8ed33
Jeff Wolfram <jeffwolfram@gmail.com>
2017-04-14 13:34:38 -0700

    added edit and delete

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -20,6 +20,32 @@ class WikisController < ApplicationController
       render :new
     end
   end
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+
+    if @wiki.save
+      flash[:notice] = "Wiki was updated."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "there was an error in saving the wiki. Try again."
+      render :edit
+    end
+  end
+  def destroy
+    @wiki = Wiki.find(params[:id])
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+      redirect_to wikis_path
+    else
+      flash.now[:alert] = "There was an error deleting the wiki"
+      render :show
+    end
+  end
   private
   def wiki_params
     params.require(:wiki).permit(:title, :body, :private)
