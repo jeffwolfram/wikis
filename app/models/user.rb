41bc9805149bec4ee7899c050b0eadbfc87c15f4
Jeff Wolfram <jeffwolfram@gmail.com>
2017-04-18 14:08:11 -0700

    added roles with default of standard

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User < ApplicationRecord
   has_many :wikis, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+enum role: [:admin, :standard, :premium]
+after_initialize :set_default_role, :if => :new_record?
+
+def set_default_role
+  self.role ||= :standard
+end
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end
--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -2,4 +2,7 @@ class WikiPolicy < ApplicationPolicy
   def new?
     user.present?
   end
+  def edit
+    current_user?
+  end
 end
